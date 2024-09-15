8e9757fb6e824ebc69ca64cf18c7e9c8baf3af77
Chris Pomeroy <chris.pomeroy@hotmail.co.uk>
2024-09-15 16:53:41 -0600

    Moved insert statements to db migration

--- src/main/java/com/chris/fetch/repository/DogsRepository.java
+++ src/main/java/com/chris/fetch/repository/DogsRepository.java
@@ -1,18 +1,11 @@
 package com.chris.fetch.repository;
 
 import com.chris.fetch.model.Dog;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 
 import java.util.List;
-import java.util.Set;
 
 public interface DogsRepository extends Repository<Dog, Long> {
 
     List<Dog> findAll();
-
-    @Query("SELECT d.url FROM Dog d")
-    Set<String> findAllUrls();
-
-    void save(Dog dog);
 }
